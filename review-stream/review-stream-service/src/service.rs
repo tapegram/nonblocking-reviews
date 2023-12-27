28ce486ac5939967a615af27c379d47ee8e8baba
tavish <tapegram@gmail.com>
2023-12-27 12:39:52 -0500

    scaffold review stream

---
+++ review-stream/review-stream-service/src/service.rs
@@ -0,0 +1,20 @@
+use std::sync::Arc;
+
+use crate::{
+    //##PLOP INSERT COMMAND IMPORTS HOOK##
+};
+
+#[derive(Clone)]
+pub struct ReviewStreamService {
+    //##PLOP INSERT COMMAND HOOK##
+    // Add service infra dependencies here
+}
+
+impl ReviewStreamService {
+    pub fn new() -> Self {
+        Self {
+            //##PLOP INSERT COMMAND INSTANTIATION HOOK##
+        }
+    }
+    //##PLOP INSERT DELEGATE HOOK##
+}
