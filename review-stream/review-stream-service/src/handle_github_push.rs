0b218b59e9484b046a94a48653779f6213533e1d
tavish <tapegram@gmail.com>
2023-12-29 18:22:25 -0500

    Starting to work through mapping the github event to the domain model

--- review-stream/review-stream-service/src/handle_github_push.rs
+++ review-stream/review-stream-service/src/handle_github_push.rs
@@ -1,8 +1,11 @@
 use std::sync::Arc;
 
+use octocrab::models::webhook_events::{
+    payload::PushWebhookEventPayload, WebhookEvent, WebhookEventPayload, WebhookEventType,
+};
 use thiserror::Error;
 
-use crate::ports::push_repository::PushRepository;
+use crate::{models::Push, ports::push_repository::PushRepository};
 
 #[derive(Clone)]
 pub struct HandleGithubPush {
@@ -12,7 +15,10 @@ pub struct HandleGithubPush {
 #[derive(Clone, Debug)]
 pub struct HandleGithubPushInput {
     // Put input fields here
-    pub id: String,
+    // For lazy mapping reasons, just reusing the generic octocrab github event type and checking
+    // its push at runtime. We could add more mapping and "parse dont validate" this into the
+    // adapter.
+    pub github_event: WebhookEvent,
 }
 
 // Change the return type, if needed
@@ -20,12 +26,25 @@ pub type HandleGithubPushOutput = Result<(), HandleGithubPushFailure>;
 
 impl HandleGithubPush {
     pub async fn handle_github_push(&self, input: HandleGithubPushInput) -> HandleGithubPushOutput {
-        todo!("Implement handle_github_push")
+        let push_event: Box<PushWebhookEventPayload> = match input.github_event.specific {
+            WebhookEventPayload::Push(push_event) => push_event,
+            _ => return Err(HandleGithubPushFailure::NotAPushEvent),
+        };
+
+        todo!("")
+    }
+}
+
+impl From<Box<PushWebhookEventPayload>> for Push {
+    fn from(payload: Box<PushWebhookEventPayload>) -> Self {
+        todo!("")
     }
 }
 
 #[derive(Error, Debug, PartialEq)]
 pub enum HandleGithubPushFailure {
+    #[error("Not a push event")]
+    NotAPushEvent,
     #[error("Something went wrong")]
     Unknown(String),
 }
