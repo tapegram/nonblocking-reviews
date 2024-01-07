a18208bbb7fffff46d1be5ec569c3a461721d1a2
tavish <tapegram@gmail.com>
2024-01-07 12:37:25 -0600

    Login has a button to login via github

--- web-htmx/src/resources/auth.rs
+++ web-htmx/src/resources/auth.rs
@@ -1,12 +1,12 @@
 use crate::state::WebHtmxState;
 use crate::{components::page::PageLayout, routes};
-use axum::extract::Query;
+use axum::extract::{Query, State};
 use axum::response::{Html, IntoResponse};
 use axum::{routing::get, Router};
 
 use rscx::{component, html, props};
 use serde::Deserialize;
-use web_client::server::form::{Button, GridCell, GridLayout, Label, TextInput};
+use web_client::server::form::Button;
 
 pub fn auth_routes(state: WebHtmxState) -> Router {
     Router::new()
@@ -14,10 +14,13 @@ pub fn auth_routes(state: WebHtmxState) -> Router {
         .with_state(state)
 }
 
-async fn get_login(Query(NextUrl { next }): Query<NextUrl>) -> impl IntoResponse {
+async fn get_login(
+    State(state): State<WebHtmxState>,
+    Query(NextUrl { next }): Query<NextUrl>,
+) -> impl IntoResponse {
     Html(html! {
         <PageLayout header="Login">
-            <LoginForm login_route=routes::login() next=next/>
+            <LoginForm github_client_id=state.github_auth_config.client_id next=next/>
         </PageLayout>
     })
 }
@@ -32,7 +35,7 @@ pub struct NextUrl {
 #[props]
 struct LoginFormProps {
     #[builder(setter(into))]
-    login_route: String,
+    github_client_id: String,
 
     #[builder(setter(into))]
     next: Option<String>,
@@ -41,35 +44,8 @@ struct LoginFormProps {
 #[component]
 fn LoginForm(props: LoginFormProps) -> String {
     html! {
-        <form hx-post=props.login_route>
-            <div class="pb-12">
-                <p class="mt-1 text-sm leading-6 text-gray-600">
-                    "pssst: try user@yallchart.com / password"
-                </p>
-                <GridLayout class="mt-10">
-                    <GridCell span=4>
-                        <Label for_input="email">Email</Label>
-                        <TextInput input_type="email" name="email" autocomplete="email" />
-                    </GridCell>
-                    <GridCell span=4>
-                        <Label for_input="password">Password</Label>
-                        <TextInput input_type="password" name="password" autocomplete="password" />
-                    </GridCell>
-                    <GridCell span=4>
-                        <div class="mt-6 flex items-center justify-end gap-x-6">
-                            <Button kind="submit">Login</Button>
-                        </div>
-                    </GridCell>
-                </GridLayout>
-            </div>
-            {
-                match props.next {
-                    Some(next) => html! {
-                        <input type="hidden" name="next" value=next />
-                    },
-                    None => html! {},
-                }
-            }
-        </form>
+        <a href=format!("https://github.com/login/oauth/authorize?scope=user:email&client_id={}", props.github_client_id)>
+          Login with Github
+        </a>
     }
 }
